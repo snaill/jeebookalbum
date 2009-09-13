042d81ec51753ff2bb595a92c5e6d21c8ec10db2
com.chinaos.snaill <com.chinaos.snaill@d2fdecfe-9c85-11de-9096-1f16be7e813d>
2009-09-13 05:54:09 +0000

    提交JSuit代码

---
+++ trunk/JSuit/Class.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Globalization;
+
+namespace JSuit.ja
+{
+    [DataContract(Namespace = "http://jeebook.com/")]
+    class Exception
+    {
+        [DataMember(Name = "success")]
+        public bool success { get; set; }
+
+        [DataMember(Name = "code")]
+        public int code { get; set; }
+
+        [DataMember(Name = "message")]
+        public string message { get; set; }
+    }
+
+    [DataContract(Namespace = "http://jeebook.com/album")]
+    class Album
+    {
+        [DataMember(Name = "success")]
+        public bool success { get; set; }
+
+        [DataMember(Name = "code")]
+        public int code { get; set; }
+
+        [DataMember(Name = "message")]
+        public string message { get; set; }
+    }
+
+    [DataContract(Namespace = "http://jeebook.com/album")]
+    class CMD_albums
+    {
+        [DataMember(Name = "success")]
+        public bool success { get; set; }
+
+        [DataMember(Name = "total")]
+        public int code { get; set; }
+
+        [DataMember(Name = "albums")]
+        public Album[] albums { get; set; }
+    }
+}
---
+++ trunk/JSuit/Docbase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace JSuit
+{
+    public class Docbase
+    {
+        string m_strBase = "";
+        string m_strPrefix = "";
+        bool m_bLazy = true;
+
+        public Docbase(string strBase)
+        {
+            m_strBase = strBase;
+        }
+
+        public Docbase(string strBase, string strPrefix)
+        {
+            m_strBase = strBase;
+            m_strPrefix = strPrefix;
+        }
+
+        public Docbase(string strBase, string strPrefix, bool lazy )
+        {
+            m_strBase = strBase;
+            m_strPrefix = strPrefix;
+            m_bLazy = lazy;
+        }
+
+        public string GetDirectories(string strDir)
+        {
+            string strPath = m_strBase + strDir;
+            string strJson = strPath + m_strPrefix + "dirs.json";
+            if (!System.IO.File.Exists(strJson))
+            {
+                string[] strDirs = System.IO.Directory.GetDirectories(strPath);
+                System.IO.TextWriter tw = new System.IO.StreamWriter(strJson);
+                JsonWriter jw = new JsonTextWriter(tw);
+                jw.WriteStartArray();
+                for (int i = 0; i < strDirs.Length; i++)
+                {
+                    jw.WriteStartObject();
+                    jw.WritePropertyName("id");
+                    jw.WriteValue(strDirs[i].Substring(strPath.Length));
+                    jw.WritePropertyName("text");
+                    jw.WriteValue(strDirs[i].Substring(strPath.Length));
+                    jw.WritePropertyName("leaf");
+                    jw.WriteValue(false);
+                    jw.WriteEndObject();
+                }
+                jw.WriteEndArray();
+
+                tw.Close();
+            }
+
+            return strJson;
+        }
+    }
+}
