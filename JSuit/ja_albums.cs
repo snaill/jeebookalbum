0e6a2cdeea4939b705864c137e800f8edbfb9a94
com.chinaos.snaill <com.chinaos.snaill@d2fdecfe-9c85-11de-9096-1f16be7e813d>
2009-09-13 05:54:09 +0000

    提交JSuit代码

---
+++ JSuit/ja_albums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace JSuit.ja
+{
+    public class albums : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string strPath = context.Request.QueryString["path"];
+
+            //
+            Docbase dc = new Docbase( context.Server.MapPath("../data/"), "ja_" );
+            string dirs = dc.GetDirectories(strPath);
+
+            context.Response.WriteFile(dirs);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
